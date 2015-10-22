f426fc11280e6c432be0bb5e1959648b6951f44b
lalit sachdeva <sachdeva_libra@yahoo.com>
2015-10-22 17:17:49 +0530

    updated for search fresher jobs initial commit

---
+++ SearchFresherJobs/SearchFresherJobs/Scripts/searchFreshers/app.js
@@ -0,0 +1,48 @@
+﻿var app = angular.module('SearchFresherJobsApp', []);
+
+app.controller('homeCtrl', function ($scope) {
+
+    $scope.JobListUrls = [
+        {
+            'title': 'Vrijwilligerswerk in Amsterdam',
+            'href': 'javascript:void(0)'
+        },
+        {
+            'title': 'Vrijwilligerswerk in Amsterdam',
+            'href': 'javascript:void(0)'
+        },
+        {
+            'title': 'Vrijwilligerswerk in Amsterdam',
+            'href': 'javascript:void(0)'
+        },
+        {
+            'title': 'Vrijwilligerswerk in Amsterdam',
+            'href': 'javascript:void(0)'
+        },
+        {
+            'title': 'Vrijwilligerswerk in Amsterdam',
+            'href': 'javascript:void(0)'
+        },
+        {
+            'title': 'Vrijwilligerswerk in Amsterdam',
+            'href': 'javascript:void(0)'
+        },
+        {
+            'title': 'Vrijwilligerswerk in Amsterdam',
+            'href': 'javascript:void(0)'
+        },
+        {
+            'title': 'Vrijwilligerswerk in Amsterdam',
+            'href': 'javascript:void(0)'
+        },
+        {
+            'title': 'Vrijwilligerswerk in Amsterdam',
+            'href': 'javascript:void(0)'
+        },
+        {
+            'title': 'Vrijwilligerswerk in Amsterdam',
+            'href': 'javascript:void(0)'
+        },
+    ]
+
+});
---
+++ SearchFresherJobs/SearchFresherJobs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SearchFresherJobs.Startup))]
+namespace SearchFresherJobs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
